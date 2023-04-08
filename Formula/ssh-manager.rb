5b4d66495b16cffe6051d1414ca08990081816d2
Viktoriia Hrechukha <viktoriia.hrechukha@gmail.com>
2023-04-08 22:45:54 +0200

    [ba]: fixed url

--- Formula/ssh-manager.rb
+++ Formula/ssh-manager.rb
@@ -1,7 +1,7 @@
 class SshManager < Formula
   desc "Ssh-Manager written in Rust"
   homepage "https://github.com/vhrechukha/ssh-manager"
-  url "https://github.com/vhrechukha/ssh-manager/releases/tag/v0.1.0/ssh-manager-mac.tar.gz"
+  url "https://github.com/vhrechukha/ssh-manager/releases/download/v0.1.0/ssh-manager-mac.tar.gz"
   sha256 "4623c715b53f0079b57097a7632ecb9c8cc70e5143e979c1d2c257bcc0648866"
   version "0.1.0"
 
