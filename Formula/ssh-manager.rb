825f6348a1bc16d4c1a897f3a3dd2757e3c7a1fd
Viktoriia Hrechukha <viktoriia.hrechukha@gmail.com>
2023-04-08 23:42:35 +0200

    [ba]: fixed sha256

--- Formula/ssh-manager.rb
+++ Formula/ssh-manager.rb
@@ -2,7 +2,7 @@ class SshManager < Formula
   desc "Ssh-Manager written in Rust"
   homepage "https://github.com/vhrechukha/ssh-manager"
   url "https://github.com/vhrechukha/ssh-manager/releases/download/v0.1.0/ssh-manager-mac.tar.gz"
-  sha256 "8f5430867eff2d20e80073ed50acc6993c25b4810b1767507a18b0448bf60b03"
+  sha256 "550ffa23795cc6f1bd9ce96ec17863c8517919e5d074c974318d1567050bcd17"
   version "0.1.0"
 
   def install
