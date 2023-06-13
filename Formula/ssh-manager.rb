f9b4e18ebd22d694cff0a5fb8a8558ae3320d68c
Viktoriia Hrechukha <50745199+vhrechukha@users.noreply.github.com>
2023-06-14 01:30:36 +0300

    [ba] Update ssh-manager.rb for realease v0.2.0

--- Formula/ssh-manager.rb
+++ Formula/ssh-manager.rb
@@ -1,9 +1,9 @@
 class SshManager < Formula
   desc "Ssh-Manager written in Rust"
   homepage "https://github.com/vhrechukha/ssh-manager"
-  url "https://github.com/vhrechukha/ssh-manager/releases/download/v0.1.0/ssh-manager-mac.tar.gz"
-  sha256 "8227920fcad0a5d3abc0394fc95a77a678be58d39a43c89991faa37a81d1e6fc"
-  version "0.1.0"
+  url "https://github.com/vhrechukha/ssh-manager/releases/download/v0.2.0/ssh-manager-0.2.0-x86_64-apple-darwin.tar.gz"
+  sha256 "69f892cec4f0201103316072a6b50af98a8e9a46000e15357a44cfba15af8ce0"
+  version "0.2.0"
 
   def install
     bin.install "ssh-manager"
