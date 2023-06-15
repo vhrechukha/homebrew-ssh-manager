6dcf4575ba32b59266f515962ceac4c773ea3c34
Viktoriia Hrechukha <50745199+vhrechukha@users.noreply.github.com>
2023-06-15 17:06:54 +0300

    Update ssh-manager.rb for version 0.2.0

--- Formula/ssh-manager.rb
+++ Formula/ssh-manager.rb
@@ -2,7 +2,7 @@ class SshManager < Formula
   desc "Ssh-Manager written in Rust"
   homepage "https://github.com/vhrechukha/ssh-manager"
   url "https://github.com/vhrechukha/ssh-manager/releases/download/v0.2.0/ssh-manager-0.2.0-x86_64-apple-darwin.tar.gz"
-  sha256 "69f892cec4f0201103316072a6b50af98a8e9a46000e15357a44cfba15af8ce0"
+  sha256 "cf1dd8e37857bde26cbe972f3382edf3c5e48e4ff39e0d01d2aa594b4283cea4"
   version "0.2.0"
 
   def install
